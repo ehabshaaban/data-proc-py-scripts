3880575a1da9ffc34264d6c877c3ccc79eb818e5
EhabShaaban <ehabsfeki@gmail.com>
2020-05-04 02:32:06 +0200

    Updating error handling and refactoring

--- query-manipulation-script.py
+++ query-manipulation-script.py
@@ -1,49 +1,67 @@
+"""List:Build copy/paste/cut/select all to work probably--
+        Make query text boxes dimmed--
+        Draw error msgs/pop ups--
+        Hit enter to generate
+"""
+#Importing TKinter module
 from tkinter import *
-import os
-os.system('clear')
 
-root =Tk()
-root.title('Query Manipulation Program')
-# root.iconbitmap('/home/ehab/Desktop/query manipulation script/image.ico')
-root.geometry('400x600')
+#Setting up the GUI window
+win = Tk()
+win.title("Query Generator")
+win.resizable(0,0)
 
-#fix any other input but files!
-#and also more than two lines!!
+#Converting
 def get_queries():
+    if len(entry.get("1.0", "end-1c")) == 0:
+        empty_box = "Text Box Is Empty!"
+        print(empty_box)
+        output1.delete('1.0', END)
+        output1.insert(END, str(empty_box))
+    else: 
+        text = entry.get("1.0",END)
+        try:
+            jbase_pattern = "jbase file \[(.*?)\] with "
+            jbase_value = re.search(jbase_pattern, text).group(1)
+            oracle_pattern = "oracle file \[(.*?)]|\n"
+            oracle_value = re.search(oracle_pattern, text).group(1)
+            print("str:",text)
+            print("count:",len(text))
+            print("Jbase:", jbase_value)
+            print("Oracle:", oracle_value)
+            output1.delete('1.0', END)
+            output1.insert(END, str(jbase_value))
+        except:
+            valid_data_msg = "Enter Valid Log Data!"
+            print(valid_data_msg)
+            output1.delete('1.0', END)
+            output1.insert(END, str(valid_data_msg))
 
-    log_line = log_text_box.get()
 
-    if len(log_line) == 0:
-        print("str:",log_line)
-        print("count:",len(log_line))
-        msg = Label(root, text="Enter first line in log file", font="Tahoma").pack()
-    
-    else:
-        print("str:",log_line)
-        print("count:",len(log_line))
+def slct(event=None):
+    entry.tag_add('sel', '1.0', 'end')
+    return "break"
 
-        #finding jbase file value
-        jbase_pattern = "jbase file \[(.*?)\] with "
-        jbase_value = re.search(jbase_pattern, log_line).group(1)
-        print("jbase:", jbase_value)
-        jbase = Label(root, text=jbase_value).pack()
-        # jbase_value=None
-        
+#Creating the widgets
+l1 = Label(win, text="Enter First Line In Log:")
+entry = Text(win, width=50, height=3, wrap=WORD)
+button = Button(win, text="Generate", width=20)
+l2 = Label(win, text="Jbase Record Count Query:")
+output1 = Text(win, width=50, height=3, wrap=WORD)
 
-        #finding oracle file value
-        oracle_pattern = "oracle file \[(.*?)]|\n"
-        oracle_value = re.search(oracle_pattern, log_line).group(1)
-        print("oracle:", oracle_value)
-        oracle = Label(root, text=oracle_value).pack()
-        # oracle_value = None
+#Positioning the widgets
+l1.grid(row=1, column=1, padx=5, sticky=W)
+entry.grid(row=2, column=1, columnspan=2, padx=5, pady=(0,10))
+button.grid(row=3, column=1, columnspan=2, pady=5)
+l2.grid(row=4, column=1, padx=5, sticky=W)
+output1.grid(row=5, column=1, columnspan=2, padx=5, pady=(0,10))
 
-        log_text_box.delete(0,END)
-    
 
-log_text_box = Entry(root, width=30)
-log_text_box.insert(0, "Enter first line in log file")
-log_text_box.pack()
+#Button activation
+button.configure(command=get_queries)
 
-get_query = Button(root, text='Generate Queries',command=get_queries).pack()
+win.bind('<Control-a>',slct)
+win.bind('<Control-A>',slct)
 
-root.mainloop()
+#So the program is on repeat
+win.mainloop()
