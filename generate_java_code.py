2cecf464e7066885cfa95db56255a43a43ab3038
EhabShaaban <ehabsfeki@gmail.com>
2020-08-11 03:41:25 +0200

    Add Read Template Function Txt File for Read Function

--- generate_java_code.py
+++ generate_java_code.py
@@ -26,14 +26,11 @@ def generate_data_variables_and_enums(file_path, sheet_number):
         f2.write(ele+'\n')
     f2.close()
 
-    def generate_read_function():
-        # template_file = open("template_function.txt", "r")
-        # for aline in tempslate_file:
-        #     pass
-            # values = aline.split()
-            # print(values)
-        # template_file.close()
-        print("Hello from a function")
+def generate_read_function(MODULE_DATA,MODULE_INSTANCE,MODULE_ENUM):
+    with open("template_function.txt", "r") as a_file:
+        for line in a_file:
+            stripped_line = line.strip()
+            print(stripped_line)
 
 if __name__ == "__main__":
     """The following variables are for generating data variables in Data class & enum variables in Enum class"""
@@ -56,4 +53,4 @@ if __name__ == "__main__":
     MODULE_INSTANCE = "MODULE_INSTANCEMODULE_INSTANCE"
     # Enum class name
     MODULE_ENUM = "MODULE_ENUMMODULE_ENUM"
-    generate_read_function()
+    generate_read_function(MODULE_DATA,MODULE_INSTANCE,MODULE_ENUM)
