e43719c57e315f1cb23367c977fb9300f4b22d67
Ehab <ehabsfeki@gmail.com>
2020-11-10 20:35:10 +0200

    Add files via upload

---
+++ concat_em_all.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+import pandas as pd
+import os
+
+folder_path = ""
+report_name = "output-report-"+str(datetime.now())+".xlsx"
+files = os.listdir(folder_path)
+
+def get_path_to_file(folder_path, files):
+    
+    path_to_file_list = []
+    for f in files:
+        path_to_file = folder_path + f
+        path_to_file_list.append(path_to_file)
+    return path_to_file_list
+
+def get_it_boy(path_to_file_list):
+    
+    df = pd.DataFrame()
+    for ptf in path_to_file_list:
+        df = df.append(pd.read_excel(ptf), ignore_index=True)
+    return df.to_excel(report_name) 
+
+if __name__ == "__main__":
+    get_it_boy(get_path_to_file(folder_path, files))
