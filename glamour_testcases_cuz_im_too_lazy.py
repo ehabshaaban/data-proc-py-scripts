8ca0f1c2f05076f3e55780a6a18af2e20467a697
EhabShaaban <ehabsfeki@gmail.com>
2020-12-07 14:37:24 +0200

    :rocket: Generate excel with testcase

--- glamour_testcases_cuz_im_too_lazy.py
+++ glamour_testcases_cuz_im_too_lazy.py
@@ -1,10 +1,19 @@
 import pandas as pd
 import numpy as np
 import itertools
+from datetime import datetime
+
+now = datetime.now()
+timestamp = datetime.timestamp(now)
 
 
 df = pd.read_excel("/home/ehab/Downloads/NNII.xlsx", header=None)
 df.columns = ['TestName', 'Data']
+for i in df.index:
+    if type(df["Data"][i]) is float:
+        pass
+    else:
+        df["Data"][i] = df["Data"][i].title()
 
 # generate n/a combination
 def na_combination(l):
@@ -15,7 +24,7 @@ def make_na_list(l):
     for x in na_combination(l):
         na_list.append(''.join(x))
     return na_list
-    
+
 # save app header
 rows_meta_idx = []
 def save_app_header():
@@ -59,12 +68,18 @@ def glamour_testcases_cuz_im_too_lazy():
             df_wo_app_header.at[testcase_idx, "TestName"] = testcase_ser[testcase_idx]
 
     # glowing it all back together
-    print(df)
-    print(df_wo_app_header)
-
-# TODO: save df back to excel
+    for row in df_wo_app_header.index:
+        df.at[row, "TestName"] = df_wo_app_header["TestName"][row]
+    
+    # fill nan empty cells in 'Data' with value n/a
+    df["Data"].fillna("n/a", inplace = True)
+    
+# save DF into excel
+def save_df(df_arg):
+    df_arg.to_excel ('/home/ehab/glamour_testcases_cuz_im_too_lazy/sheet_'+str(timestamp)+'.xlsx', index = False, header=True)
     
 if __name__ == "__main__":
-    save_app_header()
+    #save_app_header()
     drop_app_header()
     glamour_testcases_cuz_im_too_lazy()
+    save_df(df)
